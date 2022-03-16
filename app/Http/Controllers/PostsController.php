0036e88bda4a20752a93bd5b2a0b2c6ecdd9d86d
Guigma <freddy.s.w.guigma@stud.h-da.de>
2022-03-16 11:41:30 +0100

    add the CRUD functions for an article

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,38 +3,61 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use DB;
 class PostsController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of all the articles.
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        //
+        //syntax with DB
+
+
+        //syntax with eloquent
+        $posts = Post::orderBy('created_at', 'desc')->get();
+        return $posts;
+
+
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Show the form for creating a new article.
      *
      * @return \Illuminate\Http\Response
      */
     public function create()
     {
-        //
+        
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Store a newly created article in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        //requirements
+        $this->validate($request, [
+            'name' => 'required',
+            'content' => 'required'
+        ]);
+
+        //create a post
+
+        $post = new Post;
+
+        $post->name = $request->name;
+        $post->content = $request->content;
+        //$post->user_id=Auth::user()->id;
+        $post->user_id = $request->user_id;
+        $post->save();
+
+
     }
 
     /**
@@ -45,7 +68,8 @@ class PostsController extends Controller
      */
     public function show($id)
     {
-        //
+        $post = Post::find($id);
+        return $post;
     }
 
     /**
@@ -56,7 +80,13 @@ class PostsController extends Controller
      */
     public function edit($id)
     {
-        //
+        $post = Post::find($id);
+
+        if(!isset($post)){
+            return redirect('/posts')->with('error' , 'No Post Found');
+        }
+
+        return $post;
     }
 
     /**
@@ -68,7 +98,26 @@ class PostsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        //requirements
+        $this->validate($request, [
+            'name' => 'required',
+            'content' => 'required'
+        ]);
+
+        
+
+        $post =  Post::find($id);
+
+        //Update a post
+
+
+        $post->name = $request->input('name');
+        $post->content = $request->input('content');
+        $post->user_id = $request->input('user_id');
+        $post->save();
+
+        return redirect('/posts')->with('success','Post updated');
+
     }
 
     /**
@@ -79,6 +128,15 @@ class PostsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+
+        //check for existing articles before deleting
+        if(!isset($post)){
+            return redirect('/posts')->with('error' , 'No Post Found');
+        }
+
+        $post->delete();
+
+        return redirect('/posts')->with('success' , 'Succesfully deleted');
     }
 }
