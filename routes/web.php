e980a27936e7718498da2a9105a8c88d77b3b386
Guigma <freddy.s.w.guigma@stud.h-da.de>
2022-03-16 13:27:09 +0100

    configure the routes of my application

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+ 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\PostsController;
+
+Route::get('/', 'App\Http\Controllers\PagesController@index');
+Route::resource('posts' ,PostsController::class );
