00ff9044f771d4df09cb0aaf01150b0eed3d3ea7
Guigma <freddy.s.w.guigma@stud.h-da.de>
2022-03-16 10:52:14 +0100

    create Model Post to manage a post. Add the function Autor to specify the relationship between user and post

---
+++ app/Models/Post.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    /**
+     * A post(article) belongs to exact one user
+     */
+    public function autor()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
